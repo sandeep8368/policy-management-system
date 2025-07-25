a8c273b5fedb2f3469fca6948647e261f0771a6c
Sandeep Kumar <sk0870997@gmail.com>
2025-07-25 12:40:00 +0530

    configured MySQL and JWT in settings

--- policy_project/settings.py
+++ policy_project/settings.py
@@ -37,7 +37,20 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'insurance',
+    'rest_framework_simplejwt',
 ]
+
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
+
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -74,8 +87,12 @@ WSGI_APPLICATION = 'policy_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'insurance_db',
+        'USER' : 'root',
+        'PASSWORD' : '8368',
+        'HOST' : 'localhost',
+        'PORT' : '3306',
     }
 }
 
