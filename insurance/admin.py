9db93754b7cb32769d664d4cdcfc3d46bf694cf4
Sandeep Kumar <sk0870997@gmail.com>
2025-07-25 13:31:29 +0530

    Added prefetch_related for optimization

--- insurance/admin.py
+++ insurance/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from django import forms
+from .models import Policy
+
+class PolicyForm(forms.ModelForm):
+    class Meta:
+        class Meta:
+            model = Policy
+            fields = '__all__'
